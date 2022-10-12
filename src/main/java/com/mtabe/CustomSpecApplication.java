893f77323b0fb6df8b1908c9c8bf2a21e2143c1b
Zachariah Ngonyani <zachariahn@gmail.com>
2022-10-12 19:56:55 +0300

    feat: initial commit

---
+++ src/main/java/com/mtabe/CustomSpecApplication.java
@@ -0,0 +1,13 @@
+package com.mtabe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomSpecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomSpecApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mtabe/CustomSpecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mtabe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomSpecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
